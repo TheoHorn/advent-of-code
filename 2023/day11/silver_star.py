02547ae5c0a36788244202f7063fdd35a76dc1eb
Theo Hornberger <theo.hornberger@telecomnancy.eu>
2023-12-11 14:28:39 +0100

    day11

---
+++ 2023/day11/silver_star.py
@@ -0,0 +1,49 @@
+with open ('2023/day11/input.txt') as f:
+    lines = f.readlines()
+    lines = [line.rstrip() for line in lines]
+    line_empty = []
+    column_empty = []
+    for i, line in enumerate(lines):
+        if line.find('#') == -1:
+            line_empty.append(i)
+    
+    for i in range(len(lines[0])):
+        column = ''
+        col = column.join([line[i] for line in lines])
+        if col.find('#') == -1:
+            column_empty.append(i)
+
+    stars = []
+    for i, line in enumerate(lines):
+        offset = 0
+        while line:
+            if line.find('#', offset) != -1:
+                stars.append((i, line.find('#', offset)))
+                offset = line.find('#', offset) + 1
+            else:
+                break
+    ans = 0
+    marqued = []
+    for i in range(len(stars)):
+        marqued.append(stars[i])
+        for j in range(i + 1, len(stars)):
+            if stars[j] in marqued:
+                continue
+            else:
+                dist_x = 0
+                dist_y = 0
+                for k in range(min(stars[i][0], stars[j][0]), max(stars[i][0], stars[j][0])):
+                    if k in line_empty:
+                        dist_x += 1
+                    dist_x += 1
+                for k in range(min(stars[i][1], stars[j][1]), max(stars[i][1], stars[j][1])):
+                    if k in column_empty:
+                        dist_y += 1
+                    dist_y += 1
+                ans += dist_x + dist_y
+
+print(ans)
+    
+
+
+    
