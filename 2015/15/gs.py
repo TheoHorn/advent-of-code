3de6985b3d258914c3a475ba76dbfdc1749e018a
Theo Hornberger <theo.hornberger@telecomnancy.eu>
2024-12-05 18:39:36 +0100

    Add solutions for Day 6 of Advent of Code 2015

---
+++ 2015/15/gs.py
@@ -0,0 +1,43 @@
+import re
+from itertools import permutations
+
+def scores(array, repartition):
+    values = [0] * (len(array[0])-1)
+    calories = 0
+    for i, quantity in enumerate(repartition):
+        for j in range(len(values)):
+            values[j] += quantity * array[i][j]
+        calories += quantity * array[i][-1]
+
+    if calories != 500: 
+        return 0
+    if any(value < 0 for value in values):
+        return 0
+    result = 1
+    for value in values:
+        result *= value
+    return result
+
+
+with open("2015/15/input.txt") as f:
+    lines = f.readlines()
+    array = []
+    for line in lines:
+        matches = re.findall(r'-?\d+', line)
+        matches = [int(x) for x in matches]
+        array.append(matches)  
+
+    nb_teespoon = 100
+    num_ingredients = len(array)
+    best_score = 0
+
+    for repartition in permutations(range(nb_teespoon + 1), num_ingredients):
+        if sum(repartition) == nb_teespoon:
+            score = scores(array, repartition)
+            if score > best_score:
+                best_score = score
+                print(f"New best repartition: {repartition}, score: {best_score}")
+
+    print("Best score:", best_score)
+
+
