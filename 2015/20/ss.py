144aeccd08dcaaeda87440267540b9eb0d21984d
Theo Hornberger <theo.hornberger@telecomnancy.eu>
2024-12-13 18:11:48 +0100

    Add solutions for Day 20 of Advent of Code 2015

---
+++ 2015/20/ss.py
@@ -0,0 +1,26 @@
+val = 34000000
+
+def find_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    return divisors
+
+def nb_presents(divisors,house):
+    presents = 0
+    for divisor in divisors:
+        mult = house // divisor
+        presents += mult*10
+    return presents
+
+
+presents = 0
+house = 1
+while(presents < val):
+    divisors = find_divisors(house)
+    presents = nb_presents(divisors,house)
+    house += 1
+print(house-1)
